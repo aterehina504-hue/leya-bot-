204fc1fd9884cf61f969e9d530b226440a40de29
aterehina504-hue <aterehina504@gmail.com>
2026-01-31 15:32:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ async def start_webserver():
     runner = web.AppRunner(app)
     await runner.setup()
 
-    port = int(os.getenv("PORT", 10000))
+    port = int(os.getenv("PORT", 8080))
     site = web.TCPSite(runner, "0.0.0.0", port)
     await site.start()
 
